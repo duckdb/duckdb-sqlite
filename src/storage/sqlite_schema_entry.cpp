3203bbc2d3d786c66ae7b7f96720c46dbe0b20a9
Alex Kasko <alex@staticlibs.net>
2026-05-28 20:16:09 +0100

    Submodule bump

--- src/storage/sqlite_schema_entry.cpp
+++ src/storage/sqlite_schema_entry.cpp
@@ -72,8 +72,8 @@ optional_ptr<CatalogEntry> SQLiteSchemaEntry::CreateFunction(CatalogTransaction 
 void UnqualifyColumnReferences(ParsedExpression &expr) {
 	if (expr.GetExpressionType() == ExpressionType::COLUMN_REF) {
 		auto &colref = expr.Cast<ColumnRefExpression>();
-		auto name = std::move(colref.column_names.back());
-		colref.column_names = {std::move(name)};
+		auto name = std::move(colref.ColumnNamesMutable().back());
+		colref.ColumnNamesMutable() = {std::move(name)};
 		return;
 	}
 	ParsedExpressionIterator::EnumerateChildren(expr, UnqualifyColumnReferences);
